70ef09453dbf67d23fd6b4bda38ea84ac0b9cf89
Sébastien <seb-canale@hotmail.frpwd>
2025-11-04 16:06:23 +0100

    backend avec anti casse et format de date

--- routes/trips.js
+++ routes/trips.js
@@ -7,19 +7,21 @@ const Trip = require ("../Models/trips")
 
 
 // route get : donne les trains disponibles en fonction des paramètres 
-router.get("/", (req, res) => {
-    const {departure, arrival, date} = req.body
+router.post("/", (req, res) => {
+    const {departure, arrival, date} = req.body; 
     console.log(departure)
     if (!departure || !arrival || !date) { 
         res.json ({result: false, error :"le trajet n'existe pas"})
         return 
     }
 
+    const formatDate = new Date(date)
 Trip.find({
-    departure,
-    arrival,
-    date
+    departure: {$regex: new RegExp(departure, 'i')},
+    arrival: {$regex: new RegExp(arrival, 'i')}, 
+    date : { $gte: formatDate} 
 })
+
 .then(tripData => res.json({result: true,  message: "le trajet a été trouvé", trip: tripData  }))
 
 })
