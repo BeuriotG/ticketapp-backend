a9af2d8e4b1e2c33446b2610faadc21fca8fd599
Sébastien <seb-canale@hotmail.frpwd>
2025-11-04 14:27:00 +0100

    route get marche

---
+++ routes/trips.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+require("../Models/connection")
+const Trip = require ("../Models/trips")
+
+
+
+
+// route get : donne les trains disponibles en fonction des paramètres 
+router.get("/", (req, res) => {
+    const {departure, arrival, date} = req.body
+    console.log(departure)
+    if (!departure || !arrival || !date) { 
+        res.json ({result: false, error :"le trajet n'existe pas"})
+        return 
+    }
+
+Trip.find({
+    departure,
+    arrival,
+    date
+})
+.then(tripData => res.json({result: true,  message: "le trajet a été trouvé", trip: tripData  }))
+
+})
+
+module.exports = router;
